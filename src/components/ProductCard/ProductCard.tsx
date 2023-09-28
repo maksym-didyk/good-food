a20e84120e64e6c6b750eb2cb8f1fa34e1600a8a
Maksym Didyk <m.didyk@itg-investments.com>
2023-09-28 18:53:31 +0300

    added product page

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,19 +3,24 @@ import './ProductCard.scss';
 import imgPlan from '../../assets/images/light.png';
 import { Link } from 'react-router-dom';
 
-export const ProductCard = () => {
+interface Props {
+  small?: boolean
+}
+
+export const ProductCard: React.FC <Props> = ({ small = false }) => {
     return(
         <div className='product-card'>
           <div className='product-card__container'>
             <img src={imgPlan} alt='Light'/>
             <div className='product-card__title'>Light</div>
-            <div className='product-card__calories'>1200 ккал</div>
-            <div className='product-card__content'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
-            <div className='product-card__price'>€ 24</div>
-            <div className='product-card__button'>
-              <Link to='/menu' className='product-card__button-buy'>Заказать</Link>
-            </div>
-            
+            {small || (
+              <>
+                <div className='product-card__calories'>1200 ккал</div>
+                <div className='product-card__content'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div><div className='product-card__price'>€ 24</div><div className='product-card__button'>
+                  <Link to='/menu' className='product-card__button-buy'>Заказать</Link>
+                </div>
+              </>
+            )}
           </div>
         </div>
     );
